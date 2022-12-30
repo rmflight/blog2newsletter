4ccff4bcad4b37fd6a14946279f207b31693a926
rmflight <r.flight79@gmail.com>
2022-12-30 10:06:45 -0500

    getting the root directory and creating the cache properly

--- R/manage_metadata.R
+++ R/manage_metadata.R
@@ -1,11 +1,34 @@
+#' create cache
+#'
+#' Creates the cache directory to be used by `blog2newsletter`.
+#'
+#' @param cache the subdirectory to use
+#'
+#' @export
+#' @return invisible
 create_cache_directory = function(cache = "_blog2newsletter")
 {
-  NULL
+  root_dir = rprojroot::find_root(proj_or_pkg_root)
+  cache_loc = file.path(root_dir, cache)
+  dir_made = dir.create(cache_loc)
+  if (dir_made) {
+    cli::cli_alert_success("Created cache directory {.file {cache_loc}}.")
+  } else {
+    message(cli::format_message(c("x" = "Couldn't create cache directory {.file {cache_loc}}.",
+                          "i" = "Do you have write permissions in this directory?")))
+  }
+  invisible()
 }
+
+proj_or_pkg_root = rprojroot::root_criterion(function(path){
+  file.exists(file.path(path, "DESCRIPTION")) || file.exists(Sys.glob(file.path(path, "*.Rproj")))
+},
+  "has DESCRIPTION or .Rproj")
 
 check_for_cache = function(cache = "_blog2newsletter")
 {
-  root_dir = rprojroot::find_root(rprojroot::is_rstudio_project)
+
+  root_dir = rprojroot::find_root(proj_or_pkg_root)
   if (dir.exists(file.path(root_dir, cache)) || file.exists(file.path(root_dir, "DESCRIPTION"))) {
     return(TRUE)
   } else {
